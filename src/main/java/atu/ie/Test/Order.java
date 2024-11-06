4b4e7997db21734a9bab7582999504cf4815d5a6
AARONPOWDERLY-STUDEN <G00434425@atu.ie>
2024-11-06 17:32:24 +0000

    Added ArrayList of Order objects

--- src/main/java/atu/ie/Test/Order.java
+++ src/main/java/atu/ie/Test/Order.java
@@ -3,32 +3,45 @@ import java.util.ArrayList;
 import java.util.Scanner;
 public class Order {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter Your name");
+        String name1 = sc.nextLine();
+        name s1 = new name();
+        s1.setname(name1);
+        System.out.println("name entered: " s1.getName);
+
+        ArrayList<String> products = new ArrayList<String>();
+
+        products.add("Toaster");
+        products.add("Phone");
+        products.add("Kettle");
+        products.add("Keyboard");
+        products.add("Monitor");
 
         System.out.println("Please enter your oderId: ");
         boolean validInput = false;
 
-        while(!validInput){
+        while (!validInput) {
             try {
                 String Input = sc.nextLine();
                 int myNum = Integer.parseInt(Input);
-            if(myNum >= 0&& myNum <= 3) {
-                System.out.println("You have ordered" + myNum);
-                validInput = true;
-            }else{
-                System.out.println("You have not entered a valid orderId");
-            } catch(NumberFormatException ae){
+                if (myNum >= 0 && myNum <= 5) {
+                    System.out.println("You have ordered" + products.get(0));
+                    validInput = true;
+                    System.out.println("There is 12 Toaster left in stock");
+                } else {
+                    System.out.println("You have not entered a valid orderId");
+                } catch(NumberFormatException ae){
                     System.out.println("Invalid orderId. Please enter another orderId");
                 }
+            }
+
         }
 
-        System.out.println("Please enter Your name");
-        String name1 = sc.nextLine();
-        Customer n1 = new Customer();
-        n1.set Name(name1);
 
 
     }
 
 
-}
+
